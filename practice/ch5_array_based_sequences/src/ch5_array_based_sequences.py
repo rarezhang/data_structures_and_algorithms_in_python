372b40308c1acf69f6c51b9a40b601c317f95eae
rarezhang <catezhang@qq.com>
2016-11-27 00:15:35 +0000

    ch5.6

--- practice/ch5_array_based_sequences/src/ch5_array_based_sequences.py
+++ practice/ch5_array_based_sequences/src/ch5_array_based_sequences.py
@@ -309,10 +309,84 @@ print('answer: ', answer)
 column = 3
 row = 3
 data = ([0]*column)*row  # can only get 1 dimension list with column*row elements
-print(data)
+print(data)  # [0, 0, 0, 0, 0, 0, 0, 0, 0]
 data = [[0]*column]*row  # all row entries of the list know as data are references to the same instance of a list of column zero
 data[2][0] = 100  # e.g., will get [[100, 0, 0], [100, 0, 0], [100, 0, 0]]
 print(data)
+# [[100, 0, 0], [100, 0, 0], [100, 0, 0]]
 data = [[0] * column for j in range(row)]
 data[2][0] = 100  # will get [[0, 0, 0], [0, 0, 0], [100, 0, 0]]
 print(data)
+# [[0, 0, 0], [0, 0, 0], [100, 0, 0]]
+
+
+# P 245
+# multidimensional data set
+class TicTacToe:
+    """
+    management of a Tic Tac toe game
+    does not do strategy
+    """
+
+    def __init__(self):
+        """
+        start a new game
+        """
+        self._board = [[' '] * 3 for j in range(3)]
+        self._player = 'X'
+
+    def mark(self, i, j):
+        """
+        put an X or O mark at position (i,j) for next player's turn
+        :param i:
+        :param j:
+        :return:
+        """
+        if not (0 <= i <= 2 and 0<= j <= 2):
+            raise ValueError("invalid board position")
+        if self._board[i][j] != ' ':
+            raise ValueError("board position occupied")
+        if self.winner() is not None:
+            raise ValueError("Game is already complete")
+        self._board[i][j] = self._player
+        if self._player == "X":
+            self.player = "O"
+        else:
+            self._player = "X"
+
+    def _is_win(self, mark):
+        """
+        Check whether the board configuration is a win for the given player.
+        :param mark:
+        :return:
+        """
+        board = self._board  # local variable for shorthand
+        return (mark == board[0][0] == board[0][1] == board[0][2] or # row 0
+                mark == board[1][0] == board[1][1] == board[1][2] or # row 1
+                mark == board[2][0] == board[2][1] == board[2][2] or # row 2
+                mark == board[0][0] == board[1][0] == board[2][0] or # column 0
+                mark == board[0][1] == board[1][1] == board[2][1] or # column 1
+                mark == board[0][2] == board[1][2] == board[2][2] or # column 2
+                mark == board[0][0] == board[1][1] == board[2][2] or # diagonal
+                mark == board[0][2] == board[1][1] == board[2][0]) # rev diagonal
+
+    def winner(self):
+        """
+
+        :return: mark of winning player or None to indicate a tie
+        """
+        for mark in 'XO':
+            if self._is_win(mark):
+                return mark
+            return None
+
+    def __str__(self):
+        """
+
+        :return: string representation of current game board
+        """
+        rows = ['|'.join(self._board[r]) for r in range(3)]
+        return '\n-----\n'.join(rows)
+
+
+
